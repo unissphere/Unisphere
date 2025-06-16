d26368b92ec59577722065e6f93656b384a1d2d5
nomrat09 <nimratkr256@gmail.com>
2025-06-16 07:59:07 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+      <div className="flex justify-end gap-4 px-8 py-4">
+        <button className="bg-rose-100 text-rose-700 rounded-full px-4 py-1 shadow-sm hover:bg-rose-200 transition">whats there</button>
+        <button className="bg-rose-100 text-rose-700 rounded-full px-4 py-1 shadow-sm hover:bg-rose-200 transition">about us</button>
+      </div>
+    );
+  }
