55c1e8749dee433f59ecaa0c4dbc4c0c33fe1d8d
sachi-sr <sachisri05@gmail.com>
2025-06-17 13:53:00 +0530

    Moved main.py and config.py to backend folder

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
 from authlib.integrations.starlette_client import OAuth, OAuthError
-from config import CLIENT_ID, CLIENT_SECRET
+from config import CLIENT_ID, CLIENT_SECRET 
 from fastapi.staticfiles import StaticFiles 
 
 app = FastAPI()
